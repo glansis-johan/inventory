fc9786cbceb3123425627ac8618c3e98ea33e17b
Johan Staaf <johan@Johans-iMac.local>
2018-11-19 19:07:33 +0100

    table updates

--- development/se.glansis.inventory.database.definitions/dbTable.cs
+++ development/se.glansis.inventory.database.definitions/dbTable.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Collections.Generic;
 using se.glansis.inventory.database;
 
-namespace se.glansis.inventory.domain
+namespace se.glansis.inventory.database.definitions
 {
-    public static class UserTable
+    public class DbTable
     {
-
-        private Dictionary<string, string> _columns = new Dictionary<string, string>();
+        private string _tableName;
+ 
+        private Dictionary<string, string> _columnDefinitions = new Dictionary<string, string>();
+        private Dictionary<string, object> _columnData = new Dictionary<string, object>();
         private long _id;
         private string _code;
         private string _name;
@@ -15,9 +18,8 @@ namespace se.glansis.inventory.domain
         private string _changedby;
         private DateTime _changed;
         private long _rowversion;
-        private string _login;
-        private int _password;
-
+ 
+        public string TableName { get => _tableName; set => _tableName = value; }
         public long Id { get => _id; set => _id = value; }
         public string Code { get => _code; set => _code = value; }
         public string Name { get => _name; set => _name = value; }
@@ -26,13 +28,11 @@ namespace se.glansis.inventory.domain
         public string Changedby { get => _changedby; set => _changedby = value; }
         public DateTime Changed { get => _changed; set => _changed = value; }
         public long Rowversion { get => _rowversion; set => _rowversion = value; }
-        public string Login { get => _login; set => _login = value; }
-        public int Password { get => _password; set => _password = value; }
-
+ 
 
     }
 
-    public UserTable()
+    public DbTable()
     {
     }
 
@@ -51,14 +51,14 @@ namespace se.glansis.inventory.domain
       return true;
     }
 
-    public User FindById(int id)
+    public Dictionary<string, object> FindById(int id)
     {
-      return new User();
+      return _columnData ;
     }
 
-    public User FindByCode(string code)
+    public Dictionary<string, object> FindByCode(string code)
     {
-      return new User();
+      return _columnData;
     }
 
 
